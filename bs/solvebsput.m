d0b3b7d9b9292f2e101c722f0202a5cf90ef60f5
liugoanywhere <liugoanywhere@gmail.com>
2017-07-19 00:47:44 +0800

    1.3

--- bs/solvebsput.m
+++ bs/solvebsput.m
@@ -2,9 +2,14 @@ function [sigma,fval,exitflag,output] = solvebsput( s,k,y,t,r  )
 %SOLVEBSCALL Summary of this function goes here
 %   Detailed explanation goes here
 options = optimoptions(@fsolve,'Display','off','Jacobian','off','TolFun',1e-10);
- [sigma,fval,exitflag,output]=fsolve(@(x)bsput( s,k,x,t,r )-y,0.3,options);
-
+for i=1:len(s)
+    
+[sigma1,fval1,exitflag1,output1]=fsolve(@(x)bsput( s(i),k(i),x(i),t(i),r(i) )-y(i),0.3,options);
+sigma(i)=sigma1;
+fval(i)=fval1;
+exitflag(i)=exitflag1;
+output(i)=output1;
+end
 
 
 end
-
